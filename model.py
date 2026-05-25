cbc585db4627a9404cc738e69d9d2f81b0deac50
DoKKa9bi <82722483+DoKKa9bi@users.noreply.github.com>
2026-05-25 10:45:22 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 #Данный файл содержит непосредственно описание модели
 
 #CNN для распознавания дипфейков
-#Архитектура для классов RotIris, RotBall и RotEye:
-#  Фото  -> CNN(1) -> Выделение области глаз и радужки в два потока -> CNN по области глаз(2)(4 слоя) -> активация, субдескритизация, выпрямление, полносвязный -> 
+#Архитектура для класса RotEyes:
+#  Фото  -> CNN(1) -> Выделение области глаз, глазного яблока и радужки в три потока -> CNN по области глаз(2)(4 слоя) -> активация, субдескритизация, выпрямление, полносвязный -> 
 #                                                                   -> CNN по радужке(2)(4 слоя)      -> активация, субдескритизация, выпрямление, полносвязный -> 
 #																	-> CNN по области глаза(2)(4 слоя)-> активация, субдескритизация, выпрямление, полносвязный -> 
 #        -> Если на входе видео, то проходим в несколько итераций, по полученному массиву значений, ReLU с bias=0,7 и (3)-> выводим на градиентный спуск три полученых значения и полученный (3), получаем результат
@@ -35,16 +35,59 @@ def see_eyes()
 class RotEyes(nn.Module):
 	def __init__(self, num_classes=1):
 		super().__init__()
-		self.RotArea=self.__make_branch()
-		self.RotEye=self.__make_branch()
-		self.RotIris=self.__make_branch()
+		self.RotArea=self.branch()
+		self.RotEye=self.branch()
+		self.RotIris=self.branch()
 
+	def branch(self):
+		reurn nn.Sequental(
+		nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #128x64 -> 64x32
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #64x32 -> 32x16
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #32x16 -> 32x16
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+
+			nn.AdaptiveAvgPool2d((1, 1))  
+	)
+
+	def fuse(self):
+		return nn.Sequental(
+			nn.Linear(256 * 3, 128)
+            nn.ReLU(inplace=True)
+            nn.Dropout(0.3)
+            nn.Linear(128, num_classes)
+		)
+		
+	def forward(self, area: torch.Tensor, eye: torch.Tensor, iris: torch.Tensor):
+		f_area = self.RotArea(area).view(area.size(0), -1)
+        f_eye  = self.RotEye(eye).view(eye.size(0), -1)
+        f_iris  = self.RotTris(iris).view(iris.size(0), -1)
+        
+        # Позднее объединение
+        fused = torch.cat([f_whole, f_lids, f_iris], dim=1)  # (B, 768)
+        return self.fusion(fused)                        
 		
 #Макет, на всю область глаз, 6 слоёв
 class RotCNN(nn.Module):
 	def __init__(self, num_classes=1):
 	super().__init__()
-	self.features = nn.Sequential(
+	self.features nn.Sequential(
             #256x128 -> 128x64
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
