aebd135bb00a150d209d422f389a64b85df0a6e5
DoKKa9bi <82722483+DoKKa9bi@users.noreply.github.com>
2026-05-25 10:26:18 +0300

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,8 @@
 #Архитектура для классов RotIris, RotBall и RotEye:
 #  Фото  -> CNN(1) -> Выделение области глаз и радужки в два потока -> CNN по области глаз(2)(4 слоя) -> активация, субдескритизация, выпрямление, полносвязный -> 
 #                                                                   -> CNN по радужке(2)(4 слоя)      -> активация, субдескритизация, выпрямление, полносвязный -> 
-#        -> Если на входе видео, то проходим в несколько итераций, по полученному массиву значений, ReLU с bias=0,7 и (3)-> выводим на градиентный спуск два полученых значения и полученный (3), получаем результат
+#																	-> CNN по области глаза(2)(4 слоя)-> активация, субдескритизация, выпрямление, полносвязный -> 
+#        -> Если на входе видео, то проходим в несколько итераций, по полученному массиву значений, ReLU с bias=0,7 и (3)-> выводим на градиентный спуск три полученых значения и полученный (3), получаем результат
 # (1) CNN обучается распознавать по области глаз и радужки отдельно
 # (2) Обучение на итоговой выборке, нужна вероятность того, что данное изображение принадлежит дипфейку
 # (3) Для видео мы можем получить динамику EAR, потому мы можем, по сути создать эрзац-свёрточную функцию, в которой мы применяем метод скользящего окна на 20 значений через Max Pooling, с шагом в 5, и подаём на Байесовский
@@ -30,61 +31,62 @@ IMGHEIGHT=128
 #Выделение массива с тремя областями из входного изображения
 def see_eyes()
 
-#Область глаза
-class RotEye(nn.Module):
-	def __init__(self, learning_rate=0.001, num_classes=2):
-	
-#Зона, ограниченная веками
-class RotBall(nn.Module):
-	def __init__(self, learning_rate=0.001, num_classes=2):
-	
-#Зона радужки
-class RotIris(nn.Module):
-	def __init__(self, learning_rate=0.001, num_classes=2):
-	
-#Макет на всю область глаз
-class RotCNN(nn.Module):
-	def __init__(self, learning_rate=0.001, num_classes=2,):
-	self.relu=nn.ReLU()
-	self.model = self.init_model()
-        optimizer = Adam(learning_rate = learning_rate)
-        self.model.compile(optimizer = optimizer, loss = 'mean_squared_error', metrics = ['accuracy'])
-    
-	def init_model(self):
-	x=Input(shape=(IMGWIDTH,IMGHEIGHT,3))
-	conv1=nn.Conv2d(64,(6,6),padding=0, strides=1)(x) #256 - 128 -3
-	conv1=nn.BatchNorm2d(8)(conv1)
-	conv1=self.relu(conv1)
-	
-	conv2=nn.Conv2d(32,(3,3),padding=1, strides=1)(conv1)
-	conv2=nn.BatchNorm2d(8)(conv2)
-	conv2=self.relu(conv2)
-	
-	conv3=nn.Conv2d(32,(3,3),padding=2, strides=1)(conv2)
-	conv3=nn.BatchNorm2d(6)(conv3)
-	conv3=self.relu(conv3)
-	
-	conv4=nn.Conv2d(16,(2,2),padding=1, strides=1)(conv3)
-	conv4=nn.BatchNorm2d(6)(conv4)
-	conv4=self.relu(conv4)
-	
-	conv5=nn.Conv2d(8,(2,2),padding=1, strides=1)(conv4)
-	conv5=nn.BatchNorm2d(4)(conv5)
-	conv5=self.relu(conv5)
-	
-	conv6=nn.Conv2d(4,(2,2),padding=2, strides=1)(conv5)
-	conv6=nn.BatchNorm2d(4)(conv6)
-	conv6=self.relu(conv6)
-	
-	nn.MaxPool2d(kernel_size=(2, 2))(y)
-	nn.Dropout2d(0.5)(y)
-	
-	        
-        y = Flatten()(x4)
-        y = Dropout(0.5)(y)
-        y = Dense(16)(y)
-        y = LeakyReLU(negative_slope=0.1)(y)
-        y = Dropout(0.5)(y)
-        y = Dense(1, activation = 'sigmoid')(y)
+#Область глаза, три ветви, 6 слоёв
+class RotEyes(nn.Module):
+	def __init__(self, num_classes=1):
+		super().__init__()
+		self.RotArea=self.__make_branch()
+		self.RotEye=self.__make_branch()
+		self.RotIris=self.__make_branch()
 
-        return KerasModel(inputs = x, outputs = y)
+		
+#Макет, на всю область глаз, 6 слоёв
+class RotCNN(nn.Module):
+	def __init__(self, num_classes=1):
+	super().__init__()
+	self.features = nn.Sequential(
+            #256x128 -> 128x64
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #128x64 -> 64x32
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #64x32 -> 32x16
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #32x16 -> 32x16
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+
+			#32x16 -> 16x8
+			nn.Conv2d(256, 256, 3, padding=1)
+			nn.GroupNorm2d(256)
+			nn.ReLU(inplace=True)	
+			nn.MaxPool2d(2)
+		
+            nn.AdaptiveAvgPool2d((1, 1))  
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.4),
+            nn.Linear(256, 64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+            nn.Linear(64, num_classes)  
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)  
+        x = self.classifier(x)
+        return x
